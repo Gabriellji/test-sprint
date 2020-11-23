7aec35fffe9647eb2411a667a3cb494ee36da8cb
Gabriellji <gabriellji@yahoo.com>
2020-11-23 23:06:41 +0300

    feat: render cards on main page

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,2 +1,14 @@
+import React from 'react';
+import './Card.css';
 
+const Card = ({ category, description, image, price, title }) => (
+    <div className="card_wrap">
+        <p>{category}</p>
+        <h1>{title}</h1>
+        <img src={image} alt={category} />
+        <p>{price}</p>
+        <p>{description}</p>
+    </div>
+)
 
+export default Card;
--- src/components/cardlist/CardList.js
+++ src/components/cardlist/CardList.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import CommerceService from '../../services/commerce-service';
+import Card from '../card/Card';
 
 class CardList extends Component {
 
     commerceService = new CommerceService();
 
     state = {
-        cardList: null
+        cardList: null,
+        loading: true
     };
 
     componentDidMount() {
@@ -15,15 +17,36 @@ class CardList extends Component {
         .then((cardList) => {
             console.log(cardList)
             this.setState({
-                cardList
+                cardList,
+                loading: false
             });
-        });
+        })
+        .catch(console.error());
+    }
+
+    renderItems(arr) {
+        return arr.map(({ id, category, description, image, price, title }) => {
+            return <Card
+            key={id}
+            category={category}
+            description={description}
+            image={image}
+            price={price}
+            title={title}
+            />
+        })
     }
 
     render() {
+
+        const { cardList, loading } = this.state;
+        console.log(cardList)
+
+        //const items = this.renderItems(cardList);
+
         return (
             <div className="card-list_wrap">
-
+                {!loading && this.renderItems(cardList)}
             </div>
         )
     }
