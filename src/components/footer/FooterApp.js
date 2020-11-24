b81152eb2cb0b4ea0c4de9dc0ee45f2c48401923
Teiko <teikomancini1993@gmail.com>
2020-11-24 14:25:10 +0100

    main footer with basic css done

--- src/components/footer/FooterApp.js
+++ src/components/footer/FooterApp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FooterLinks from "./FooterLinks/FooterLinks";
 import FooterDescription from "./FooterDescription/FooterDescription";
+import "./Footer.css"
 
 function FooterApp() {
   return (
