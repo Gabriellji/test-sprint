ec20d8641f7551d09926abc00dfe44558a2b5db8
Teiko <teikomancini1993@gmail.com>
2020-11-24 18:06:03 +0100

    login form working. will refractor tomorrow

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,7 @@ import ElectronicPage from "../electronicPage/ElectronicPage";
 import MisPage from "../misPage/MisPage";
 import RegisterPage from "../registerPage/RegisterPage";
 import FooterApp from "../footer/FooterApp";
-import {LoginData} from "../data/LoginData"
+import { LoginData } from "../data/LoginData";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -25,10 +25,10 @@ class App extends Component {
       loginPage: false,
       isLoggedIn: false,
       username: "",
-      password:"",
+      password: "",
     };
   }
-  navbarToggle = (clickedTab) =>
+  navbarToggle = clickedTab =>
     this.setState({
       homePage: false,
       foodPage: false,
@@ -39,21 +39,39 @@ class App extends Component {
       loginPage: false,
       [clickedTab]: true,
     });
-  loginToggle = () => this.setState({
-    loginPage: true
-  })
-  usernameChange = username => this.setState({username: username.target.value})
-  passwordChange = password => this.setState({password: password.target.value})
-  loginClick = (event) => {
-    event.preventDefault()
-    for (let i=0; i<LoginData.length; i++){
-      this.state.username===LoginData[i].username && this.state.password===LoginData[i].password 
-      ?/* this.setState({isLoggedIn:true}) */
-      console.log("yes")
-      :/*  alert("wrong username/password") */
-      console.log("no")
+
+  loginToggle = () =>
+    this.setState({
+      loginPage: true,
+    });
+
+  usernameChange = username =>
+    this.setState({ username: username.target.value });
+
+  passwordChange = password =>
+    this.setState({ password: password.target.value });
+
+  loginClick = event => {
+    event.preventDefault();
+    if(LoginData.find(user => user.username === this.state.username && user.password === this.state.password)) {
+      this.setState({ isLoggedIn: true })
+    } else {
+      alert("nope");
     }
-  }
+  }; /* this.setState({isLoggedIn:true}) */ /*  alert("wrong username/password") */ /* console.log("yes")
+      : */ /* console.log("no") */
+  /* {
+      user.username === this.state.username &&
+        user.password === this.state.password &&
+        this.setState({ isLoggedIn: true });
+    });!this.state.isLoggedIn && alert("nope")
+  };  */ /* this.state.username===LoginData[i].username && this.state.password===LoginData[i].password
+      ? */ /*  LoginData.filter(user =>
+        (
+          user.username===this.state.username && user.password===this.state.password
+          ? console.log("yes")
+          :console.log("no")
+        )) */
 
   render() {
     return (
@@ -71,14 +89,26 @@ class App extends Component {
           </ul>
         </nav>
 
-        {this.state.loginPage === true && (
+        {this.state.isLoggedIn ? (
+          <div>Hello {this.state.username}</div>
+        ) : (
           <form>
-            <input placeholder="Username" value={this.state.username} onChange={this.usernameChange.bind(this)}></input>
-            <input placeholder="Password" value={this.state.password} onChange={this.passwordChange.bind(this)}></input>
-            <button onClick={this.loginClick} type="submit">Login</button>
-        </form>
+            <input
+              placeholder="Username"
+              value={this.state.username}
+              onChange={this.usernameChange.bind(this)}
+            ></input>
+            <input
+              placeholder="Password"
+              value={this.state.password}
+              onChange={this.passwordChange.bind(this)}
+            ></input>
+            <button onClick={this.loginClick} type="submit">
+              Login
+            </button>
+          </form>
         )}
-        
+
         {this.state.homePage === true && (
           <main>
             <h1>i am the homepage</h1>
